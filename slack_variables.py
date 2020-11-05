e92c4c79c56a520dc0137b956856efeb8de3708a
notmarshmllow <60079743+notmarshmllow@users.noreply.github.com>
2020-11-06 02:41:18 +0530

    Update slack_variables.py

--- slack_variables.py
+++ slack_variables.py
@@ -1,1 +1,9 @@
-slack_webhook_url = 'https://hooks.slack.com/services/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'
+#Go to api.slack.com
+#Select your App
+#Select Add features and functionality > Incoming Webhooks
+#Below on the page you will find your Slack webhook url
+#Copy and Paste you Slack Webhook URL in place of _slack_webhook_url_here
+#slack_webhook_url = 'https://hooks.slack.com/services/XXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXX'
+
+
+slack_webhook_url = '_slack_webhook_url_here'
