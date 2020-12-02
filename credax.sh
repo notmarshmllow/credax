14b2bbbb984d28a25b649d78d9d60744aeb919f0
notmarshmllow <60079743+notmarshmllow@users.noreply.github.com>
2020-12-02 22:25:06 +0530

    Update credax.sh

--- credax.sh
+++ credax.sh
@@ -1,1 +1,1 @@
-cat $1 | while read domain;do python3 credax.py -d $domain -w $2 -s -o $domain_credax.txt; done
+cat $1 | while read domain;do python3 credax.py -d $domain -w $2 -s -o $domain_credax.txt; don
