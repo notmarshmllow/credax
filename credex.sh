49e94dbd259d2fb6795c924c8c76b544a0f05ff7
notmarshmllow <60079743+notmarshmllow@users.noreply.github.com>
2020-12-02 18:26:45 +0530

    Update credex.sh

--- credex.sh
+++ credex.sh
@@ -1,17 +1,1 @@
-echo "RUNNING GAU TO FETCH URLs"
-
-gau $1 > gau.txt | cat gau.txt | grep -aoP "(?<=(\"|\'|\`))\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))" | sort -u > endpoints.txt;
-
-echo "ENDPOINTS FETCHED SUCCESSFULLY."
-
-echo "RUNNING SPRAWL TO CREATE A WORDLISTS."
-
-while read word;do echo $word | ./sprawl/sprawl.py -s >> wordlists.txt;done
-
-cat endpoints.txt wordlists.txt | sort -u > wordlist.txt
-
-echo "WORDLIST CREATED SUCCESSFULLY."
-sleep(2)
-echo "RUNNING CREDAX ..."
-sleep(1)
-python3 credax.py -d $1 -w wordlists.txt -o $1_credax_results.txt -s
+cat $1 | while read domain;do python3 credax.py -d $domain -w $2 -s -o $domain_credax.txt; done
